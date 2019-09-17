9017cb104c2fc676b403f04553e62a78b6fcbe19
wulangzhou <1207175256@qq.com>
2019-09-17 19:33:03 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8 -*-
+import LD
+
+print LD.edit_dis_fast('fads', 'fdasj')
+print LD.edit_dis('fads', 'fdasj')
+print LD.edit_dis('发的数据库', '大神讲解')
+print LD.edit_path('发的数据库', '大神讲解')
