f20df68640909389a692db0e3b32b48ed8a74777
wulangzhou <1207175256@qq.com>
2019-09-17 19:32:30 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# file: setup.py
+from distutils.core import setup, Extension
+
+module1 = Extension('LD',
+                    define_macros = [('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
+                    #include_dirs = ['./include'],
+                    #library_dirs = ['./src'],
+                    #libraries = ['StringUtils.cpp'],
+                    sources = ['edit.cpp'])
+
+setup(name = 'yw_ld',
+      version = '1.0.1',
+      author = 'wulangzhou',
+      author_email = 'wulangzhou@yuewen.com',
+      url = 'https://docs.python.org/extending/building',
+      description = 'is edit distance demo',
+      ext_modules= [module1])
