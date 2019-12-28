dfec26a4b77f853384d6201ff1b9881709b0fc23
祝青瑞 <zhuqingrui@yuewen.com>
2019-12-29 00:57:04 +0800

    小修改

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 # -*- coding: utf8 -*-
 import LD
 
-print LD.edit_dis_fast('fads', 'fdasj')
+print LD.edit_dis('fa', 'fd')
+print LD.edit_dis('', 'fd')
+print LD.edit_dis('f', 'fd')
+print LD.edit_dis('f', 'f')
+print LD.edit_dis('fads', 'fdasj')
 print LD.edit_dis('fads', 'fdasj')
 print LD.edit_dis('发的数据库', '大神讲解')
 print LD.edit_path('发的数据库', '大神讲解')
