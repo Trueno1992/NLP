89ed65bf4ca28197068a9b6bb6b3ec920da6d224
wulangzhou <1207175256@qq.com>
2020-04-19 23:47:42 +0800

    稳定版

--- suggest/temp.cpp
+++ suggest/temp.cpp
@@ -1,4 +1,17 @@
 #include<iostream>
+# include <map>
+# include <vector>
+# include <stdio.h>
+# include <iostream>
+# include <pthread.h>
+# include <unistd.h>    // sleep
+# include <string.h>
+# include <stdint.h>    // uint32_t
+# include <utility>     // 包含pair
+# include <queue>
+# include <algorithm>
+
+
 using namespace std;
 
 template<class W, class T>
@@ -37,10 +50,46 @@ public:
         cout<<"b"<<endl;
     }
 };
+
+template<class W> bool a_gt_b(const W &a, const W &b){
+    if(a >= b) return true;
+    return false;
+}
+
+
+template<class W, bool (*gt)(const W &a, const W &b)>
+class Node{
+public:
+    bool a_gt_b_test(W a, W b){
+        return gt(a, b);
+    }
+public:
+    int a;
+};
+
+template<class W, bool (*gt)(const W &a, const W &b)=a_gt_b>
+class SonNode: public Node<W, gt>{
+public:
+    SonNode(){
+    }
+    //bool compare(W, W);
+public:
+    int c;
+};
+
+/*
+template<class W, bool (*gt)(const W &a, const W &b)=a_gt_b>
+bool SonNode<W, gt>::compare(W a, W b){
+    return gt(a, b);
+}
+*/
+
+
 int main(){
     //node2<int, int> *n2 = new node2<int, int>(1, 2);
     //node1<int, int> *n1 = new node1<int, int>(n2);
     //cout<<n1->w<<endl;
+    /*
     try{
         int a = 10;
         int k = 1/0;
@@ -51,5 +100,11 @@ int main(){
     }catch(...){
         throw;
     }
+    */
+    exit(0);
+    SonNode<int> son;
+    //cout<<son.compare(4, 5)<<endl;
+    //cout<<son.compare(4, -5)<<endl;
+    cout<<"fds"<<endl;
     return 0;
 }
